733a4258c604ef88468faaf7391351794f10196f
TakamakWasTaken <gautier.nicollet@ynov.com>
2019-11-07 10:55:09 +0100

    Ajout de l'architecture générale

--- app/src/main/java/com/example/partymaker/MainActivity.kt
+++ app/src/main/java/com/example/partymaker/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,13 +18,39 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun startInscription(view: View){
-        val intention = Intent(this, InscriptionActivity::class.java)
-        startActivity(intention)
+        val intentionInscription = Intent(this, InscriptionActivity::class.java)
+        startActivity(intentionInscription)
     }
 
     //Check user identifiers, if tokens are matching connect him and start eventListActivity. If tokens are wrong, inform user and stay put.
     fun connect(view: View){
+        val email = input_connectMail.text.toString()
+        val pass = input_connectPass.text.toString()
 
 
+        if(checkUserCredentials(email, pass)){
+            val intentionConnection = Intent(this, EvenementListActivity::class.java)
+            intentionConnection.putExtra("connectedUserMail", email)
+            intentionConnection.putExtra("connectedUserPass", pass)
+
+            startActivity(intentionConnection)
+        }
+    }
+
+    private fun checkUserCredentials(email: String, pass: String): Boolean{
+        var connect = false
+
+        if(pass.length > 3 && isEmailValid(email)){
+            connect = true
+        }
+        else{
+            toast("Email ou mot de passe incorrect!")
+        }
+
+        return connect
+    }
+
+    private fun isEmailValid(email: CharSequence): Boolean {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 }
